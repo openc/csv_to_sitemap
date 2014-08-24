d84bd4308a9b48e54a1f00127a0cc2d622a6457c
Countculture <countculture@gmail.com>
2014-08-24 12:08:03 +0100

    Fix multiple bugs.
    Remove debugger refs
    Use correct XML for sitemap index
    Use /sitemap path for links to sitemaps
    Cope with bad rows (poss commas in company_number?)

--- lib/csv_to_sitemap.rb
+++ lib/csv_to_sitemap.rb
@@ -1,5 +1,5 @@
 require "csv_to_sitemap/version"
-require 'debugger'
+# require 'debugger'
 require 'csv'
 
 module CsvToSitemap
@@ -7,21 +7,21 @@ module CsvToSitemap
 
   XML_INDEX_WRAPPER_START = <<-HTML
 <?xml version="1.0" encoding="UTF-8"?>
-  <sitemapindex
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-      http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  >
+<sitemapindex
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+    http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+>
   HTML
 
   XML_INDEX_WRAPPER_END = "</sitemapindex>"
 
   XML_WRAPPER_START = <<-HTML
 <?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-    xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 HTML
 
   XML_WRAPPER_END = "</urlset>"
@@ -29,6 +29,11 @@ HTML
   ENTRIES_PER_FILE = 40000
 
   def add_to_sitemap_file(row_data, row_counter)
+    # skip non-dates, and log fact
+    unless row_data[:lastmod][/^\d\d\d\d-\d\d-\d\d/]
+      puts "Bad row data: #{row_data}"
+      return
+    end
     if row_counter%ENTRIES_PER_FILE == 0
       if @current_sitemap_file
         @current_sitemap_file.puts XML_WRAPPER_END
@@ -55,23 +60,21 @@ HTML
     @export_dir = options[:export_dir]||'/tmp'
     @protocol = options[:protocol]||'http'
     @file_counter = row_counter = file_byte_counter = 0
-    # debugger
     @sitemap_index_file = File.new(File.join(@export_dir, 'sitemap.xml'),'w')
-    @sitemap_index_file.puts XML_WRAPPER_START
+    @sitemap_index_file.puts XML_INDEX_WRAPPER_START
     CSV.foreach(csv_file, :headers => true) do |row|
-      # debugger
       add_to_sitemap_file(convert_row(row), row_counter)
       row_counter += 1
     end
     @current_sitemap_file.close
-    @sitemap_index_file.puts XML_WRAPPER_END
+    @sitemap_index_file.puts XML_INDEX_WRAPPER_END
     @sitemap_index_file.close
   end
 
   def sitemap_index_entry(counter)
     entry = <<-HTML
     <sitemap>
-      <loc>http://#{@domain}/sitemap#{counter}.xml.gz</loc>
+      <loc>http://#{@domain}/sitemap/sitemap#{counter}.xml.gz</loc>
       <lastmod>#{Time.now.iso8601.sub(/Z$/i, '+00:00')}</lastmod>
     </sitemap>
     HTML
--- lib/csv_to_sitemap/version.rb
+++ lib/csv_to_sitemap/version.rb
@@ -1,3 +1,3 @@
 module CsvToSitemap
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
